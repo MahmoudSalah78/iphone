b2d42c8d49f4dd3d97e250fe3bad7e675a24cd8a
MahmoudSalah78 <166469945+MahmoudSalah78@users.noreply.github.com>
2024-04-09 05:46:25 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let img = document.getElementById('img');
+let main = document.querySelector('.container');
+
+function phones(x){
+    img.src = x;
+}
+function color(cool){
+    main.style.background = cool;
+}
